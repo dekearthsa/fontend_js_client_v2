5710906da483f048358500db4482fd0e0c6f99eb
pcsishun <pcsishun@gmail.com>
2024-03-27 15:45:29 +0700

    upate

--- src/views/AlwayDryView.vue
+++ src/views/AlwayDryView.vue
@@ -222,9 +222,11 @@ const haddleBtnOnOff = () => {
                                         <path fill-rule="evenodd" d="M5.636 4.575a.75.75 0 0 1 0 1.061 9 9 0 0 0 0 12.728.75.75 0 1 1-1.06 1.06c-4.101-4.1-4.101-10.748 0-14.849a.75.75 0 0 1 1.06 0Zm12.728 0a.75.75 0 0 1 1.06 0c4.101 4.1 4.101 10.75 0 14.85a.75.75 0 1 1-1.06-1.061 9 9 0 0 0 0-12.728.75.75 0 0 1 0-1.06ZM7.757 6.697a.75.75 0 0 1 0 1.06 6 6 0 0 0 0 8.486.75.75 0 0 1-1.06 1.06 7.5 7.5 0 0 1 0-10.606.75.75 0 0 1 1.06 0Zm8.486 0a.75.75 0 0 1 1.06 0 7.5 7.5 0 0 1 0 10.606.75.75 0 0 1-1.06-1.06 6 6 0 0 0 0-8.486.75.75 0 0 1 0-1.06ZM9.879 8.818a.75.75 0 0 1 0 1.06 3 3 0 0 0 0 4.243.75.75 0 1 1-1.061 1.061 4.5 4.5 0 0 1 0-6.364.75.75 0 0 1 1.06 0Zm4.242 0a.75.75 0 0 1 1.061 0 4.5 4.5 0 0 1 0 6.364.75.75 0 0 1-1.06-1.06 3 3 0 0 0 0-4.243.75.75 0 0 1 0-1.061ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z" clip-rule="evenodd" />
                                     </svg>
                                 </div>
-                                <div class="flex border-2 border-[#66B6AB] h-[30px] pl-2 pr-2 text-[#66B6AB] rounded-md">
-                                    <div class="translate-y-[5px] mr-1"><SpotLogoVue/></div>
-                                    <div class="translate-y-[4px]  text-[12px]">ON</div>
+                                <div class="flex border-2 border-[#66B6AB]  pl-3 pr-3 pt-1 pb-1 text-[#66B6AB] rounded-md">
+                                    <div class="mr-1">
+                                        <img src="@/assets/spot_on.png" height="20" width="20"/>
+                                    </div>
+                                    <div class="translate-y-[2px] font-bold text-[10px]">ON</div>
                                 </div>
                             </div>
                         </div>
